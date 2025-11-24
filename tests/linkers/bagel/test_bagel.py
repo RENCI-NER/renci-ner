ea75cd19fd3f8cf6efa9f65dd24ce06736c27091
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-11-24 01:31:30 -0500

    Yeah, fixed the Bagel test and even made the text actually accurate, yay me.

--- tests/linkers/bagel/test_bagel.py
+++ tests/linkers/bagel/test_bagel.py
@@ -29,7 +29,7 @@ def test_check():
         pytest.skip(f"BioMegatron is not available: {err}")
         return
 
-    text = "In orbital cellulitis, an infection travels through the ophthalmic vein into the eye socket."
+    text = "In orbital cellulitis, tissues behind the orbital septum become inflamed."
     annotated_text = biomegatron.annotate(text)
 
     try:
@@ -48,109 +48,157 @@ def test_check():
             "limit": 1,
             # Make this as predictable/repeatable as possible.
             "temperature": 0,
-            "top_p": 0.5,
+            "top_p": 0.9,
         },
     )
     assert result.text == annotated_text.text
     assert result.annotations == [
         Annotation(
-            text="orbital cellulitis",
+            text="orbital",
             id="MONDO:0006881",
             label="orbital cellulitis",
             type="biolink:Disease",
             start=3,
-            end=21,
+            end=10,
             provenance=bagel.provenance,
             based_on=[
                 Annotation(
-                    text="orbital cellulitis",
+                    text="orbital",
                     id="I1-",
                     label="",
-                    type="biolink:Disease",
-                    start=3,
-                    end=21,
-                    provenance=biomegatron.provenance,
-                    based_on=[],
-                    props={},
-                )
-            ],
-            props={
-                "description": "Inflammation of the eye tissues posterior to the orbital septum, and generally secondary to an infection spread from adjacent sinuses. Signs and symptoms of the affected eye include sudden loss of vision, erythema, edema, decreased eye movement, and pain. Treatment is conducted via intravenous antibiotics, observation, and surgical intervention when necessary.",
-            },
-        ),
-        Annotation(
-            text="infection",
-            id="UMLS:C3714514",
-            label="Infection",
-            type="biolink:Disease",
-            start=26,
-            end=35,
-            provenance=bagel.provenance,
-            based_on=[
-                Annotation(
-                    text="infection",
-                    id="I4-",
-                    label="",
-                    type="biolink:Disease",
-                    start=26,
-                    end=35,
-                    provenance=biomegatron.provenance,
-                    based_on=[],
-                    props={},
-                )
-            ],
-            props={
-                "description": "",
-            },
-        ),
-        Annotation(
-            text="ophthalmic vein",
-            id="UBERON:0011191",
-            label="ophthalmic vein",
-            type="biolink:GrossAnatomicalStructure",
-            start=56,
-            end=71,
-            provenance=bagel.provenance,
-            based_on=[
-                Annotation(
-                    text="ophthalmic vein",
-                    id="I8-",
-                    label="",
-                    type="biolink:GrossAnatomicalStructure",
-                    start=56,
-                    end=71,
-                    provenance=biomegatron.provenance,
-                    based_on=[],
-                    props={},
-                )
-            ],
-            props={
-                "description": "Ophthalmic veins are veins which drain the eye. More specifically, they can refer to: Superior ophthalmic vein Inferior ophthalmic vein.",
-            },
-        ),
-        Annotation(
-            text="eye socket",
-            id="UMLS:C2371860",
-            label="Structure of eye socket",
-            type="biolink:AnatomicalEntity",
-            start=81,
-            end=91,
-            provenance=bagel.provenance,
-            based_on=[
-                Annotation(
-                    text="eye socket",
-                    id="I12-",
-                    label="",
                     type="biolink:AnatomicalEntity",
-                    start=81,
-                    end=91,
+                    start=3,
+                    end=10,
                     provenance=biomegatron.provenance,
                     based_on=[],
                     props={},
                 ),
             ],
             props={
-                "description": "",
+                "description": "Inflammation of the eye tissues posterior to the orbital "
+                "septum, and generally secondary to an infection spread from "
+                "adjacent sinuses. Signs and symptoms of the affected eye "
+                "include sudden loss of vision, erythema, edema, decreased "
+                "eye movement, and pain. Treatment is conducted via "
+                "intravenous antibiotics, observation, and surgical "
+                "intervention when necessary.",
+            },
+        ),
+        Annotation(
+            text="cellulitis",
+            id="MONDO:0005230",
+            label="cellulitis",
+            type="biolink:Disease",
+            start=11,
+            end=21,
+            provenance=bagel.provenance,
+            based_on=[
+                Annotation(
+                    text="cellulitis",
+                    id="I2-",
+                    label="",
+                    type="biolink:Disease",
+                    start=11,
+                    end=21,
+                    provenance=biomegatron.provenance,
+                    based_on=[],
+                    props={},
+                ),
+            ],
+            props={
+                "description": "Inflammation of the dermis and subcutaneous tissues caused "
+                "by a bacterial infection. Symptoms include erythema, edema, "
+                "and pain to the affected area.",
+            },
+        ),
+        Annotation(
+            text="tissues",
+            id="UBERON:0000479",
+            label="tissue",
+            type="biolink:GrossAnatomicalStructure",
+            start=23,
+            end=30,
+            provenance=bagel.provenance,
+            based_on=[
+                Annotation(
+                    text="tissues",
+                    id="I3-",
+                    label="",
+                    type="biolink:GrossAnatomicalStructure",
+                    start=23,
+                    end=30,
+                    provenance=biomegatron.provenance,
+                    based_on=[],
+                    props={},
+                ),
+            ],
+            props={
+                "description": "Multicellular anatomical structure that consists of many "
+                "cells of one or a few types, arranged in an extracellular "
+                "matrix such that their long-range organisation is at least "
+                "partly a repetition of their short-range organisation.",
+            },
+        ),
+        Annotation(
+            text="orbital septum",
+            id="UBERON:0001822",
+            label="orbital septum",
+            type="biolink:GrossAnatomicalStructure",
+            start=42,
+            end=56,
+            provenance=bagel.provenance,
+            based_on=[
+                Annotation(
+                    text="orbital septum",
+                    id="I6-",
+                    label="",
+                    type="biolink:AnatomicalEntity",
+                    start=42,
+                    end=56,
+                    provenance=biomegatron.provenance,
+                    based_on=[],
+                    props={},
+                ),
+            ],
+            props={
+                "description": "A membranous sheet that acts as the anterior boundary of "
+                "the orbit. It extends from the orbital rims to the "
+                "eyelids[WP].",
+            },
+        ),
+        Annotation(
+            text="inflamed",
+            id="NCIT:C3137",
+            label="Inflammation",
+            type="biolink:PhenotypicFeature",
+            start=64,
+            end=72,
+            provenance=bagel.provenance,
+            based_on=[
+                Annotation(
+                    text="inflamed",
+                    id="I9-",
+                    label="",
+                    type="biolink:PhenotypicFeature",
+                    start=64,
+                    end=72,
+                    provenance=biomegatron.provenance,
+                    based_on=[],
+                    props={},
+                ),
+            ],
+            props={
+                "description": "A finding of a localized protective response resulting from "
+                "injury or destruction of tissues. Inflammation serves to "
+                "destroy, dilute, or wall off both the injurious agent and "
+                "the injured tissue. In the acute phase, inflammation is "
+                "characterized by the signs of pain, heat, redness, "
+                "swelling, and loss of function. Histologically, "
+                "inflammation involves a complex series of events, including "
+                "dilatation of arterioles, capillaries, and venules, with "
+                "increased permeability and blood flow; exudation of fluids, "
+                "including plasma proteins; and leukocyte migration into the "
+                "site of inflammation.",
             },
         ),
     ]
