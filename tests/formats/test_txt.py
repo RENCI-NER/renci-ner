0d0a30c6d31a6bcfda015e80db3979e2d30be112
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-12-22 18:36:07 -0500

    Added some additional tests.

--- tests/formats/test_txt.py
+++ tests/formats/test_txt.py
@@ -16,4 +16,12 @@ test_pmid_files = [f for f in test_pmid_dir.glob("pmid-*.txt") if f.is_file()]
 @pytest.mark.parametrize("pmid_filename", test_pmid_files)
 def test_reading_pmid_files(pmid_filename):
     texts = list(TextFile(str(pmid_filename)).read_file())
+
+    # We should have multiple texts for each PMID file.
     assert len(texts) > 0
+
+    # Check the locations within the AnnotatedTexts.
+    for text in texts:
+        assert text.location[0] == str(pmid_filename)
+        assert text.location[1] == "TextFile"
+        assert text.location[2].startswith("row=")
